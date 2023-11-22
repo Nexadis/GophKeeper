7007ee050e3d27517241440cbc805654d1b9c69c
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-11-22 11:04:44 +0300

    Добавление пользователя

--- internal/database/postgre.go
+++ internal/database/postgre.go
@@ -2,23 +2,32 @@ package database
 
 import (
 	"context"
+	"encoding/hex"
+	"errors"
+	"fmt"
 
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
 	_ "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/jmoiron/sqlx"
 
 	"github.com/Nexadis/GophKeeper/internal/config"
+	"github.com/Nexadis/GophKeeper/internal/logger"
 	"github.com/Nexadis/GophKeeper/internal/models/datas"
 	"github.com/Nexadis/GophKeeper/internal/models/users"
 )
 
+var ErrUserExist = errors.New("user is exist")
+
 type PgDB struct {
-	pg *sqlx.DB
+	db *pgxpool.Pool
 	c  *config.DBConfig
 }
 
 func Connect(ctx context.Context, c *config.DBConfig) (*PgDB, error) {
-	db, err := sqlx.ConnectContext(ctx, "pgx", c.URI)
+	db, err := pgxpool.New(ctx, c.URI)
 	if err != nil {
 		return nil, err
 	}
@@ -29,9 +38,8 @@ func Connect(ctx context.Context, c *config.DBConfig) (*PgDB, error) {
 
 }
 
-func (pg *PgDB) Close() error {
-	return pg.pg.Close()
-
+func (pg *PgDB) Close() {
+	pg.db.Close()
 }
 
 func (pg *PgDB) Add(ctx context.Context, data datas.IData) error {
@@ -50,7 +58,7 @@ func (pg *PgDB) DeleteByID(ctx context.Context, id int) error {
 	return nil
 }
 func (pg *PgDB) Ping(ctx context.Context) error {
-	return pg.pg.PingContext(ctx)
+	return pg.db.Ping(ctx)
 }
 func (pg *PgDB) GetUserByID(ctx context.Context, id int) (users.User, error) {
 	return nil, nil
@@ -60,6 +68,27 @@ func (pg *PgDB) GetUserByName(ctx context.Context, username string) (users.User,
 }
 
 func (pg *PgDB) AddUser(ctx context.Context, u users.User) error {
+	query := `INSERT INTO users (username, hash, created_at) VALUES (@username, @hash, @created_at) RETURNING id`
+	args := pgx.NamedArgs{
+		"username":   u.Username(),
+		"hash":       hex.EncodeToString(u.Hash()),
+		"created_at": u.CreatedAt(),
+	}
+	logger.Debug(fmt.Sprintf("Add user %s in db", u.Username()))
+	res := pg.db.QueryRow(ctx, query, args)
+	var id int
+	err := res.Scan(&id)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
+
+			return ErrUserExist
+		}
+		return fmt.Errorf("problem with db: %w", err)
+	}
+	u.SetID(id)
+	logger.Debug(fmt.Sprintf("Added user %s with id %d", u.Username(), id))
+
 	return nil
 
 }
