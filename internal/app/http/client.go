0e18ffcd47eb64d33158653b9351e926433f6258
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-11-26 18:43:26 +0300

    Методы для обращения к серверу через HTTP

--- internal/app/http/client.go
+++ internal/app/http/client.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Nexadis/GophKeeper/internal/config"
 	"github.com/Nexadis/GophKeeper/internal/logger"
+	"github.com/Nexadis/GophKeeper/internal/models/datas"
 )
 
 var ErrInvalidAuth = errors.New(`invalid login or password`)
@@ -50,12 +51,16 @@ func (hc *Client) Login(ctx context.Context, login, password string) error {
 	if err != nil {
 		return fmt.Errorf("Problems with connection: %w", err)
 	}
-	switch resp.StatusCode() {
-	case http.StatusForbidden:
-		return fmt.Errorf("Can't auth: %w", ErrInvalidAuth)
-	case http.StatusBadRequest:
-	case http.StatusInternalServerError:
-		return fmt.Errorf("Error on server %w: %s", ErrServerProblem, (resp.Body()))
+	if resp.StatusCode() != http.StatusOK {
+		switch resp.StatusCode() {
+		case http.StatusForbidden:
+			return fmt.Errorf("Can't auth: %w", ErrInvalidAuth)
+		case http.StatusBadRequest:
+			fallthrough
+		case http.StatusInternalServerError:
+			return fmt.Errorf("Error on server %w: %s", ErrServerProblem, (resp.Body()))
+
+		}
 	}
 	token := getToken(resp.Header())
 	hc.client.SetAuthScheme("Bearer")
@@ -87,5 +92,66 @@ func (hc *Client) Register(ctx context.Context, login, password string) error {
 	hc.client.SetAuthScheme("Bearer")
 	hc.client.SetAuthToken(token)
 	logger.Info(fmt.Sprintf("User %s successful registered, token=%s", login, token))
+	return nil
+}
+
+func (hc Client) GetData(ctx context.Context) ([]datas.Data, error) {
+	var ds []datas.Data
+	resp, err := hc.client.R().
+		SetContext(ctx).
+		SetResult(&ds).
+		Get(hc.config.Address + APIv1 + APIData)
+	if err != nil {
+		return nil, fmt.Errorf("Problems with connection: %w", err)
+	}
+	switch resp.StatusCode() {
+	case http.StatusNotFound:
+		logger.Debug("User doesn't have data")
+	case http.StatusBadRequest:
+		fallthrough
+	case http.StatusInternalServerError:
+		return nil, fmt.Errorf("Can't get data: %w - %s", ErrServerProblem, (resp.Body()))
+
+	}
+	return ds, nil
+}
+
+func (hc Client) PostData(ctx context.Context, dlist []datas.Data) error {
+	resp, err := hc.client.R().
+		SetContext(ctx).
+		SetBody(dlist).
+		Post(hc.config.Address + APIv1 + APIData)
+	if err != nil {
+		return fmt.Errorf("Problems with connection: %w", err)
+	}
+	if resp.StatusCode() == http.StatusBadRequest {
+		return fmt.Errorf("Can't post data: %s", resp.Body())
+	}
+	return nil
+}
+func (hc Client) UpdateData(ctx context.Context, dlist []datas.Data) error {
+	resp, err := hc.client.R().
+		SetContext(ctx).
+		SetBody(dlist).
+		Patch(hc.config.Address + APIv1 + APIData)
+	if err != nil {
+		return fmt.Errorf("Problems with connection: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return fmt.Errorf("Can't update data: %s", resp.Body())
+	}
+	return nil
+}
+func (hc Client) DeleteData(ctx context.Context, ids []int) error {
+	resp, err := hc.client.R().
+		SetContext(ctx).
+		SetBody(ids).
+		Delete(hc.config.Address + APIv1 + APIData)
+	if err != nil {
+		return fmt.Errorf("Problems with connection: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return fmt.Errorf("Can't delete data: %s", resp.Body())
+	}
 	return nil
 }
