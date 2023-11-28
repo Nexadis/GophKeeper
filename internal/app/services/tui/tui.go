fb41828ebf9ad6026cd81fe8831825fa07b7ab43
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-11-28 15:51:05 +0300

    Приветствие для пользователя. Структура на базе страниц.

--- internal/app/services/tui/tui.go
+++ internal/app/services/tui/tui.go
@@ -8,6 +8,9 @@ import (
 	"github.com/Nexadis/GophKeeper/internal/models/datas"
 )
 
+const HelloMessage = `Hello, this is GophKeeper - Application for save and modify your data.
+You can sign up in the system and your notes will be saved.`
+
 type Connection interface {
 	Login(ctx context.Context, login, password string) error
 	Register(ctx context.Context, login, password string) error
@@ -20,22 +23,46 @@ type Connection interface {
 
 type Tui struct {
 	app           *tview.Application
+	pages         *tview.Pages
+	currentPage   int
 	loginAttempts int
 	err           error
 	c             Connection
 }
 
 func NewTui(c Connection) *Tui {
-
+	pages := tview.NewPages()
 	app := tview.NewApplication()
-	return &Tui{
+	t := &Tui{
 		app,
+		pages,
+		0,
 		0,
 		nil,
 		c,
 	}
+	pageList := []pageFunc{
+		makePageFunc(t.IntroPage(HelloMessage)),
+	}
+	for i, v := range pageList {
+		name, obj := v()
+		pages.AddPage(name, obj, true, i == 0)
+
+	}
+	t.pages = pages
+	return t
+
 }
 
-func (m *Tui) Run(ctx context.Context) error {
-	return m.app.SetRoot(m.NewLoginForm(m.loginAttempts), true).Run()
+func (t *Tui) Run(ctx context.Context) error {
+	return t.app.SetRoot(t.pages, true).Run()
+}
+
+type pageFunc func() (string, tview.Primitive)
+
+func makePageFunc(name string, page tview.Primitive) pageFunc {
+	return func() (string, tview.Primitive) {
+		return name, page
+	}
+
 }
