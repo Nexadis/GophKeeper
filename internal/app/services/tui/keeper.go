610e64a5bfc7f0dc228b8014511af2e08df0fbc7
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-02 20:58:35 +0300

    Обработка изменения полей

--- internal/app/services/tui/keeper.go
+++ internal/app/services/tui/keeper.go
@@ -1,7 +1,8 @@
 package tui
 
 import (
-	"context"
+	"encoding/hex"
+	"fmt"
 	"strconv"
 
 	"github.com/gdamore/tcell/v2"
@@ -21,6 +22,7 @@ type KeeperView struct {
 	*tview.Flex
 	table      *tview.Table
 	changer    *tview.Form
+	errorShow  *tview.TextView
 	currentRow int
 }
 
@@ -29,32 +31,36 @@ func (t *Tui) KeeperPage() (string, *KeeperView) {
 	k := KeeperView{}
 	k.t = t
 
-	table := tview.NewTable().SetFixed(1, 1)
-	table.SetFocusFunc(
-		func() {
-			k.setupTable()
+	k.table = tview.NewTable().SetFixed(1, 1)
+	k.table.SetTitle("Table").SetBorder(true)
+	k.table.SetSelectable(true, false)
+	k.table.SetFocusFunc(func() {
+		if k.UntouchedList != nil {
+			return
+		}
+		k.setupTable()
+	})
+	k.table.SetBlurFunc(func() {
+		k.table.ScrollToBeginning()
+	})
+	k.changer = tview.NewForm()
 
-		},
-	)
-	table.SetTitle("Table").SetBorder(true)
-	table.SetSelectable(true, false)
-
-	editor := tview.NewFlex().AddItem(table, 0, 2, true).AddItem(tview.NewBox(), 0, 1, true)
+	editor := tview.NewFlex().AddItem(k.table, 0, 2, true).AddItem(k.changer, 0, 1, true)
 	editor.SetTitle("Edit value")
 
-	main := tview.NewFlex()
-	k.Flex = main
+	k.Flex = tview.NewFlex()
+
+	k.errorShow = tview.NewTextView()
+	k.errorShow.SetTitle("Status").SetBorder(true)
 
 	helper := k.setupKeyboard()
-	main.SetDirection(tview.FlexRow).
-		AddItem(editor, 0, 5, true).
-		AddItem(helper, 0, 1, false)
-	main.SetBorder(true).
+	k.Flex.SetDirection(tview.FlexRow).
+		AddItem(editor, 0, 10, true).
+		AddItem(k.errorShow, 0, 1, false).
+		AddItem(helper, 0, 3, false)
+	k.Flex.SetBorder(true).
 		SetTitle(title)
-	main.SetFocusFunc(func() {
 
-	})
-	k.table = table
 	return title, &k
 
 }
@@ -70,12 +76,40 @@ func (k *KeeperView) down() {
 	l := len(k.UntouchedList) + len(k.AddList) - len(k.DeleteList)
 	if k.currentRow < l {
 		k.currentRow++
+		k.table.Select(k.currentRow, 0)
 	}
-
 	logger.Debug("Down")
 }
 func (k *KeeperView) edit() {
+	var err error
+	if k.changer.GetButtonCount() != 0 {
+		k.changer.Clear(true)
+	}
+	row, _ := k.table.GetSelection()
+	data := k.getRow(row)
+	if data == nil {
+		k.errorShow.SetText(fmt.Sprintf("Can't Get data type from cell: %s", err.Error()))
+		return
 
+	}
+	k.changer.AddTextView("Type:", data.Type.String(), 0, 0, false, false)
+	k.addEditors(data)
+	k.changer.AddButton("Save", func() {
+		k.saveEdits()
+	})
+	k.changer.AddButton("Cancel", func() {
+		return
+
+	})
+
+	k.changer.SetBackgroundColor(tcell.ColorBlack)
+	k.changer.SetButtonTextColor(tcell.ColorLightGreen).
+		SetButtonBackgroundColor(tcell.ColorDarkBlue)
+	k.changer.SetFieldTextColor(tcell.ColorDarkOrange)
+	k.changer.SetFieldBackgroundColor(tcell.ColorDarkSlateGrey)
+	k.changer.SetBorder(true).SetTitle("Editor")
+	k.changer.SetFocus(1)
+	k.table.Blur()
 	logger.Debug("Edit")
 }
 func (k *KeeperView) add() {
@@ -97,75 +131,27 @@ func (k *KeeperView) delete() {
 	logger.Debugf("Delete row %d", row)
 }
 
-func (k *KeeperView) addRow(row int, line datas.Data) {
-	id := strconv.Itoa(line.ID)
-	dtype := line.Type.String()
-	val := line.Value
-	desc := line.Description
-	logger.Debugf("Add %v in table", line)
-
-	tableIDCell := tview.NewTableCell(id).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(true)
-	tableTypeCell := tview.NewTableCell(dtype).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(true).
-		SetExpansion(1)
-	tableValCell := tview.NewTableCell(val).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(true).
-		SetExpansion(1)
-	tableDescCell := tview.NewTableCell(desc).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(true).
-		SetExpansion(1)
-	k.table.SetCell(row+1, 0, tableIDCell)
-	k.table.SetCell(row+1, 1, tableTypeCell)
-	k.table.SetCell(row+1, 2, tableValCell)
-	k.table.SetCell(row+1, 3, tableDescCell)
-}
-
-func (k *KeeperView) setHeaderTable() {
-	tableIDTop := tview.NewTableCell("ID").
-		SetTextColor(tcell.ColorDarkCyan).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(false)
-	tableTypeTop := tview.NewTableCell("Type").
-		SetTextColor(tcell.ColorYellow).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(false)
-	tableValueTop := tview.NewTableCell("Value").
-		SetTextColor(tcell.ColorDarkCyan).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(false)
-	tableDescTop := tview.NewTableCell("Description").
-		SetTextColor(tcell.ColorDarkCyan).
-		SetAlign(tview.AlignCenter).
-		SetSelectable(false)
-	k.table.SetCell(0, 0, tableIDTop)
-	k.table.SetCell(0, 1, tableTypeTop)
-	k.table.SetCell(0, 2, tableValueTop)
-	k.table.SetCell(0, 3, tableDescTop)
-}
-
 func (k *KeeperView) setupKeyboard() tview.Primitive {
-	helper := tview.NewList().AddItem(
-		"up", "Up in table", 'k', nil,
+	helper := tview.NewFlex().AddItem(
+		tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewTextView().SetText("Up, k - up in table"), 0, 1, false).
+			AddItem(tview.NewTextView().SetText("Down, j - down in table"), 0, 1, false),
+		0, 1, false,
 	).AddItem(
-		"down", "Down in table", 'j', nil,
-	).AddItem(
-		"add", "Add new element in table", 'a', nil,
-	).AddItem(
-		"edit", "Edit selected element in table", 'e', nil,
-	).AddItem(
-		"delete", "Delete selected element in table", 'd', nil,
-	).AddItem(
-		"save", "Save changes on server", 's', nil,
-	)
-	helper.SetTitle("Help")
+		tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewTextView().SetText("a - add new row in table"), 0, 1, false).
+			AddItem(tview.NewTextView().SetText("e - edit choosed row in table"), 0, 1, false).
+			AddItem(tview.NewTextView().SetText("d - delete choosed row from table"), 0, 1, false),
+		0, 1, false,
+	).
+		AddItem(tview.NewTextView().SetText("ctrl+s - save changed table on server"), 0, 1, false)
+	helper.SetTitle("Help").SetBorder(true)
 
-	k.Flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	k.table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		logger.Debugf("Got %s", event.Name())
+		if !k.table.HasFocus() {
+			return event
+		}
 		switch event.Rune() {
 		case 'k':
 			k.up()
@@ -181,49 +167,74 @@ func (k *KeeperView) setupKeyboard() tview.Primitive {
 		switch event.Key() {
 		case tcell.KeyCtrlS:
 			k.updateTable()
+			return nil
 		}
 		return event
 	})
 	return helper
 }
 
-func (k *KeeperView) setupTable() {
-	var err error
-	k.UntouchedList, err = k.t.c.GetData(context.TODO())
-	if err != nil {
-		k.t.err = err
+func (k *KeeperView) addEditors(d *datas.Data) {
+	switch d.Type {
+	case datas.BankCardType:
+		number, cardHolder, expire, cvv := d.BankCardValues()
+		cvvField := strconv.Itoa(cvv)
+		k.addBankCardEditor(number, cardHolder, expire, cvvField)
+	case datas.BinaryType:
+		binValue := d.Value
+		k.addBinEditor(binValue)
+	case datas.TextType:
+		textValue := d.Value
+		k.addTextEditor(textValue)
+	case datas.CredentialsType:
+		login, password := d.CredentialsValues()
+		k.addCredsEditors(login, password)
 	}
-	k.setHeaderTable()
-	for row, line := range k.UntouchedList {
-		k.addRow(row, line)
-
-	}
-	logger.Debugf("Got %d data", len(k.UntouchedList))
+	logger.Debug("Edit values")
+	return
 }
 
-func (k *KeeperView) updateTable() {
-	logger.Debug("Update Table on server")
-	err := k.t.c.PostData(context.TODO(), k.AddList)
-	if err != nil {
-		k.t.err = err
-		logger.Errorf("Can't add data in add list:%w", err)
-		return
-	}
-	k.AddList = make([]datas.Data, 0, 10)
-	err = k.t.c.UpdateData(context.TODO(), k.UpdateList)
-	if err != nil {
-		k.t.err = err
-		logger.Errorf("Can't update data in update list:%w", err)
-	}
-	k.UpdateList = make([]datas.Data, 0, 10)
-	err = k.t.c.DeleteData(context.TODO(), k.DeleteList)
-	if err != nil {
-		k.t.err = err
-		logger.Errorf("Can't delete data in delete list:%w", err)
-	}
-	k.DeleteList = make([]int, 0, 10)
-	table := tview.NewTable().SetFixed(1, 1)
-	k.table = table
-	k.setupTable()
-	k.t.nextPage()
+func (k *KeeperView) addBankCardEditor(number, cardHolder, expire, cvvField string) {
+	k.changer.AddInputField("Number", number, 0, nil, nil)
+	k.changer.AddInputField("Card Holder", cardHolder, 0, nil, nil)
+	k.changer.AddInputField("Expire", expire, 0, nil, nil)
+	k.changer.AddInputField("CVV", cvvField, 0, nil, nil)
+
+}
+
+func (k *KeeperView) addBinEditor(bin string) {
+	k.changer.AddInputField(
+		"Binary",
+		bin,
+		0,
+		func(textToCheck string, lastChar rune) bool {
+			_, err := hex.DecodeString(string(lastChar))
+			if err != nil {
+				return false
+			}
+			return true
+
+		},
+		nil,
+	)
+
+}
+
+func (k *KeeperView) addTextEditor(text string) {
+	k.changer.AddInputField(
+		"Text",
+		text,
+		0,
+		nil, nil)
+
+}
+func (k *KeeperView) addCredsEditors(login, password string) {
+	k.changer.AddInputField("Login", login, 0, nil, nil)
+	k.changer.AddInputField("Password", password, 0, nil, nil)
+
+}
+
+func (k *KeeperView) saveEdits() {
+	logger.Debug("Save edits")
+	return
 }
