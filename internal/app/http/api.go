8876c7a8af1528b3469004aa44c431b93f1aea1a
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-07 11:59:43 +0300

    Изменения в API

--- internal/app/http/api.go
+++ internal/app/http/api.go
@@ -2,6 +2,7 @@ package http
 
 const APILogin = "/login"
 const APIRegister = "/register"
+const APIDownload = "/download/:file"
 const APIv1 = "/api/v1"
 const APIData = "/data"
 const APIPing = "/ping"
