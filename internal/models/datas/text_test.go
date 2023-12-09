4c19433d316fab5503b70924d2e5a5393d3daaa2
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-09 12:46:01 +0300

    Написал документацию по структуре text, удалил лишние, написал тесты

--- internal/models/datas/text_test.go
+++ internal/models/datas/text_test.go
@@ -1,47 +1,8 @@
 package datas
 
 import (
-	"reflect"
 	"testing"
 )
-
-func TestData_SetText(t *testing.T) {
-	type args struct {
-		value string
-	}
-	tests := []struct {
-		name    string
-		d       *Data
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.d.SetText(tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("Data.SetText() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_text_Type(t *testing.T) {
-	tests := []struct {
-		name string
-		tr   text
-		want DataType
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.tr.Type(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("text.Type() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestNewText(t *testing.T) {
 	type args struct {
@@ -52,11 +13,15 @@ func TestNewText(t *testing.T) {
 		args args
 		want *text
 	}{
-		// TODO: Add test cases.
+		{
+			"New valid text",
+			args{"text"},
+			&text{data: "text"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewText(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+			if got := NewText(tt.args.data); got.data != tt.want.data {
 				t.Errorf("NewText() = %v, want %v", got, tt.want)
 			}
 		})
@@ -64,38 +29,32 @@ func TestNewText(t *testing.T) {
 }
 
 func Test_text_Value(t *testing.T) {
+	type fields struct {
+		metaData metaData
+		data     string
+	}
 	tests := []struct {
-		name string
-		tr   text
-		want string
+		name   string
+		fields fields
+		want   string
 	}{
-		// TODO: Add test cases.
+		{
+			"Show text",
+			fields{
+				metaData{},
+				"text",
+			},
+			"text",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.tr.Value(); got != tt.want {
-				t.Errorf("text.Value() = %v, want %v", got, tt.want)
+			tr := text{
+				metaData: tt.fields.metaData,
+				data:     tt.fields.data,
 			}
-		})
-	}
-}
-
-func Test_text_SetValue(t *testing.T) {
-	type args struct {
-		value string
-	}
-	tests := []struct {
-		name    string
-		tr      *text
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.tr.SetValue(tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("text.SetValue() error = %v, wantErr %v", err, tt.wantErr)
+			if got := tr.Value(); got != tt.want {
+				t.Errorf("text.Value() = %v, want %v", got, tt.want)
 			}
 		})
 	}
