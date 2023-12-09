87880d53d34e737d5fd0239660ba590947c4cf16
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-09 11:17:00 +0300

    Убрал лишние из binary, написал тесты к ним

--- internal/models/datas/binary_test.go
+++ internal/models/datas/binary_test.go
@@ -1,102 +1,63 @@
 package datas
 
 import (
-	"reflect"
 	"testing"
+	"time"
 )
 
-func Test_binary_Type(t *testing.T) {
-	tests := []struct {
-		name string
-		b    binary
-		want DataType
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.b.Type(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("binary.Type() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewBinary(t *testing.T) {
-	type args struct {
-		data []byte
-	}
-	tests := []struct {
-		name string
-		args args
-		want *binary
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewBinary(tt.args.data); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewBinary() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_binary_Value(t *testing.T) {
-	tests := []struct {
-		name string
-		b    binary
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.b.Value(); got != tt.want {
-				t.Errorf("binary.Value() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_binary_SetValue(t *testing.T) {
-	type args struct {
-		value string
-	}
-	tests := []struct {
-		name    string
-		b       *binary
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.b.SetValue(tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("binary.SetValue() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func TestData_SetBinary(t *testing.T) {
+	type fields struct {
+		ID          int
+		UserID      int
+		Type        DataType
+		Description string
+		CreatedAt   time.Time
+		EditedAt    time.Time
+		Value       string
+	}
 	type args struct {
 		value string
 	}
 	tests := []struct {
 		name    string
-		d       *Data
+		fields  fields
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"Set invalid Hex data",
+			fields{},
+			args{
+				"invalidhex",
+			},
+			true,
+		},
+		{
+			"Set Hex data",
+			fields{
+				Value: "123",
+			},
+			args{
+				"313233",
+			},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.d.SetBinary(tt.args.value); (err != nil) != tt.wantErr {
+			d := &Data{
+				Type:        tt.fields.Type,
+				Description: tt.fields.Description,
+				Value:       tt.fields.Value,
+			}
+			err := d.SetBinary(tt.args.value)
+			if (err != nil) != tt.wantErr {
 				t.Errorf("Data.SetBinary() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			if err != nil {
+				return
+			}
+
 		})
 	}
 }
